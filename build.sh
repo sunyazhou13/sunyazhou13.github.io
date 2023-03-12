31bc6825fa48603741e63502ee1839beeca30817
sunyazhou <sunyazhou13@163.com>
2023-03-12 15:24:32 +0800

    [sunyazhou][update]1.更新 by shell

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 ./tools/build.sh
 rm _site/config.codekit3 _site/build.sh _site/Brewfile _site/Brewfile.lock.json
-rsync -auv _site onevcat@onevcat.com:/home/onevcat/www/blog
+rsync -auv _site/ ../sunyazhou13.github.io
+
+pushd `pwd`
+cd ../sunyazhou13.github.io
+echo "当前目录脚本执行目录`pwd`"
+echo "即将推送目标仓库:`git remote -v`"
+sh push.sh 
+echo "Done!"
+popd
